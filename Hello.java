455774f12a1efaf0e1ed33bcc2d06d2f76eb654c
Matheus Gusmão Lemos <33984942+matheusglemos@users.noreply.github.com>
2018-04-24 09:27:49 -0300

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,10 +1,9 @@
-
-public class Hello {
-	/**
+/**
 	* Laboratório de Programação 2 - Lab 1
 	*
 	* @author Matheus Gusmão Lemos - 117210399
 	*/
+public class Hello {
 	public static void main(String[] args){
 		System.out.println("Hello Dirlididi!");
 	}
